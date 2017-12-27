ab4d3f35b9b0874381e18946d07c2dd2a7146699
findafn <findafn@gmail.com>
2017-12-27 20:59:15 +0700

    add button search

---
+++ src/commons/ButtonSearch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+class ButtonSearch extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log('id ',this.props.idSBR);
+  }
+  render() {
+    return (
+        <Button onClick={this.handleClick}>Search</Button>
+    );   
+  }
+
+}
+
+export default ButtonSearch;
