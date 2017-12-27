94282231fc4b4b4ed57f94279899276d504f04a7
findafn <findafn@gmail.com>
2017-12-27 20:58:35 +0700

    modify input search

--- src/commons/InputSearch.js
+++ src/commons/InputSearch.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Button, Input } from 'semantic-ui-react';
+import { Input } from 'reactstrap';
 
-const InputSearch = () => (
-  <Input fluid type='text' placeholder='Masukkan ID SBR' action>
-    <input />
-    <Button type='submit'>Search</Button>
-    <Button type='submit'>Tambah</Button>
-  </Input>
-)
+class InputSearch extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange({ target }) {
+    this.props.onChangeSearch(target.value);
+  }
+  render() {
+    return (
+      <Input type='text' placeholder='Masukkan ID SBR'
+      value={this.props.idSBR} onChange={this.onChange}>
+      </Input>
+    );   
+  }
+}
 
 export default InputSearch;
