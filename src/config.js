56298120d0268e9162be62276a68e3be72a121d2
adyanf <adyanaufalf@gmail.com>
2017-12-30 14:00:39 +0700

    add config to communicate with api

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  liveSBRUrl : 'https://livesbr.adyanf.com/api',
+  sbrAuditTableUrl : 'https://sbraudittable.adyanf.com/api',
+  adauServiceUrl : 'https://adau.adyanf.com/api',
+}
